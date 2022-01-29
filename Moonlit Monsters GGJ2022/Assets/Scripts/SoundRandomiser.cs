d10e5afc3ca68155f470bab38fa964f7b8fb7b3d
dGameBoy101b <rhysmader@gmail.com>
2022-01-30 01:40:39 +0800

    Add Sound Effects
    Added power up collection sounds.
    Added enemy attack sounds.
    Fixed menu click sounds.

---
+++ Moonlit Monsters GGJ2022/Assets/Scripts/SoundRandomiser.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class SoundRandomiser : MonoBehaviour
+{
+	[SerializeField]
+	[Tooltip("The audiosource used for playing clips")]
+	private AudioSource _audioSource;
+
+	public AudioSource AudioSource
+	{
+		get
+		{
+			return this._audioSource;
+		}
+	}
+
+	[SerializeField]
+	[Tooltip("The audio clips used for footsteps")]
+	private List<AudioClip> _clips;
+
+	public ReadOnlyCollection<AudioClip> Clips
+	{
+		get
+		{
+			return this._clips.AsReadOnly();
+		}
+	}
+
+	private void Awake()
+	{
+		this._audioSource ??= this.GetComponent<AudioSource>();
+	}
+
+	private int PickClip(ICollection<AudioClip> clips)
+	{
+		return Random.Range(0, clips.Count - 1);
+	}
+
+	public void PlayClip()
+	{
+		if (this.isActiveAndEnabled)
+		{
+			this.AudioSource.PlayOneShot(this.Clips[this.PickClip(this.Clips)], 1f);
+		}
+	}
+}
